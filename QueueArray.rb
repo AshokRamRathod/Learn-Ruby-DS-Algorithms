0da02271b7e39721591001f745678f3309fb7b17
Ashok Rathode <rathodashok94@gmail.com>
2019-02-28 20:12:58 +0530

    Queue data structure implimentation using array

---
+++ QueueArray.rb
@@ -0,0 +1,42 @@
+class QueueArrayProg
+
+ "..Using Queue Data structure: Array
+  API's:
+        1.Enquue
+        2.Dequeue
+        3.IsEmpty queue "
+    
+
+  def initialize
+    @arr=[]
+  end
+
+  def enqueue elem
+    @arr.push elem
+  end
+
+  def dequeue
+    @arr.shift
+  end
+
+  def print_queue
+    puts @arr
+  end
+end
+
+queue=QueueArrayProg.new
+
+5.times do |i|
+  puts "Enter the element"
+  elem = gets
+  queue.enqueue elem
+end
+puts "Elemets are enqueue"
+queue.print_queue
+queue.dequeue
+puts "Elements are dequeue"
+queue.print_queue
+
+
+
+  
